da0850f0c7f94ccff946b8f2999864f3f76ac38f
FrancescoL96 <45236222+FrancescoL96@users.noreply.github.com>
2024-11-14 18:59:15 +0100

    fix: removed sanitize function (#28)

--- internal/database/database.go
+++ internal/database/database.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"regexp"
 	"strings"
 
 	"github.com/krateoplatformops/finops-prometheus-scraper-generic/apis"
@@ -14,27 +13,7 @@ import (
 
 const maxBatchSize = 100 // Number of records to send in each batch
 
-func sanitizeColumnValue(value string) string {
-	// Remove or replace any unsupported characters
-	unsupportedCharsRegex := regexp.MustCompile(`[^\w\s-.]`)
-	sanitizedValue := unsupportedCharsRegex.ReplaceAllString(value, "")
-
-	// Trim leading/trailing whitespace
-	sanitizedValue = strings.TrimSpace(sanitizedValue)
-
-	// Replace multiple consecutive spaces with a single space
-	sanitizedValue = strings.ReplaceAll(sanitizedValue, "  ", " ")
-
-	return sanitizedValue
-}
-
 func UploadMetrics(metrics []apis.MetricRecord, uploadServiceURL string, config apis.Config, usernamePassword *apis.UsernamePassword) error {
-	for i := range metrics {
-		for label, value := range metrics[i].Labels {
-			metrics[i].Labels[label] = sanitizeColumnValue(value)
-		}
-	}
-
 	// Split metrics into batches
 	for i := 0; i < len(metrics); i += maxBatchSize {
 		end := i + maxBatchSize
@@ -50,7 +29,7 @@ func UploadMetrics(metrics []apis.MetricRecord, uploadServiceURL string, config 
 		}
 
 		// Create request
-		url := fmt.Sprintf("%s/upload?table=%s&type=%s", uploadServiceURL, config.Exporter.TableName, config.Exporter.MetricType)
+		url := fmt.Sprintf("%s/upload?table=%s&type=%s", strings.Trim(uploadServiceURL, "/"), config.Exporter.TableName, config.Exporter.MetricType)
 		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
 		if err != nil {
 			return fmt.Errorf("error creating request: %v", err)
